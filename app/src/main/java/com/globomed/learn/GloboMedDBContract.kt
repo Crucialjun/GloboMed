4b6a76bdc148101c779ac88ea9a104f8998d5ba9
Crucialjun <nich.otieno@gmail.com>
2020-06-17 23:10:18 +0300

    Initial commit

---
+++ app/src/main/java/com/globomed/learn/GloboMedDBContract.kt
@@ -0,0 +1,24 @@
+package com.globomed.learn
+
+import android.provider.BaseColumns
+import android.provider.BaseColumns._ID
+import android.provider.Contacts.SettingsColumns.KEY
+
+object GloboMedDBContract {
+
+    object EmployeeEntry : BaseColumns {
+        const val TABLE_NAME = "employee"
+        const val COLUMN_NAME = "name"
+        const val COLUMN_DOB = "dob"
+        const val COLUMN_DESIGNATION = "designation"
+
+        const val SQL_DROP_TABLE = "DROP TABLE IF EXISTS ${EmployeeEntry.TABLE_NAME}"
+
+        const val SQL_CREATE_ENTRIES =
+            "CREATE TABLE ${EmployeeEntry.TABLE_NAME} (" +
+                    _ID  +  " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "$COLUMN_NAME TEXT NOT NULL, " +
+                    "$COLUMN_DOB INTEGER NOT NULL, " +
+                    "$COLUMN_DESIGNATION TEXT NOT NULL)"
+    }
+}
